d3b77f193c1571eb78788be34621767f78e3f267
Guillermo García <guillermieres97@gmail.com>
2022-10-26 07:53:27 +0200

    initial commit

---
+++ src/test/java/com/guillerdev97/basiccrud/BasicCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guillerdev97.basiccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
