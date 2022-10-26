d30dd036eaf5be97a784688ca65221ac7795a7ab
Guillermo García <guillermieres97@gmail.com>
2022-10-26 08:58:28 +0200

    configure app properties file to connect to db

--- src/main/java/com/guillerdev97/basiccrud/student/StudentController.java
+++ src/main/java/com/guillerdev97/basiccrud/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.guillerdev97.basiccrud.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +10,12 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/students")
 public class StudentController {
-    private final StudentService studentService;
+   private final StudentService studentService;
 
-    public StudentController(StudentService studentService) {
-        this.studentService = studentService;
-    }
+   @Autowired
+   public StudentController(StudentService studentService) {
+       this.studentService = studentService;
+   }
 
     @GetMapping
     public List<Student> list() {
--- src/main/java/com/guillerdev97/basiccrud/student/StudentService.java
+++ src/main/java/com/guillerdev97/basiccrud/student/StudentService.java
@@ -1,9 +1,13 @@
 package com.guillerdev97.basiccrud.student;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Service
 public class StudentService {
     public List<Student> list() {
         return List.of(
