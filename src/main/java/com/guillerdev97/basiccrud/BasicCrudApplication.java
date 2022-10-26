b803d33251ed32565957f7ff489b7ceed9727158
Guillermo García <guillermieres97@gmail.com>
2022-10-26 08:09:14 +0200

    create student model

--- src/main/java/com/guillerdev97/basiccrud/BasicCrudApplication.java
+++ src/main/java/com/guillerdev97/basiccrud/BasicCrudApplication.java
@@ -1,9 +1,14 @@
 package com.guillerdev97.basiccrud;
 
+import com.guillerdev97.basiccrud.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -14,9 +19,15 @@ public class BasicCrudApplication {
     }
 
     @GetMapping
-    public static String hello() {
-        return "hello world";
+    public List<Student> hello() {
+        return List.of(
+                new Student(
+                        1L,
+                        "John",
+                        "guiller@gmail.com",
+                        LocalDate.of(1990, Month.MARCH, 10),
+                        20
+                )
+        );
     }
-
-
 }
